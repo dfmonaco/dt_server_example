3595cf4aa5683257ce35ab7a4156005347fbdb87
Diego Mónaco <dfmonaco@gmail.com>
2014-06-14 20:27:18 -0300

    accesing the view_context from DatatablesServer

--- app/controllers/browsers_controller.rb
+++ app/controllers/browsers_controller.rb
@@ -2,7 +2,7 @@ class BrowsersController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.json {render json: BrowserDatatables.new(params)}
+      format.json {render json: BrowserDatatables.new(view_context)}
     end
   end
 end
--- app/datatables/browser_datatables.rb
+++ app/datatables/browser_datatables.rb
@@ -1,4 +1,10 @@
 class BrowserDatatables < DatatablesServer::Base
+  attr_reader :view
+
+  def initialize(view)
+    super(view.params)
+    @view = view
+  end
 
   def data
     Browser.all
@@ -6,6 +12,14 @@ class BrowserDatatables < DatatablesServer::Base
 
   def columns
     %w(browsers.engine browsers.browser browsers.platform browsers.version browsers.grade)
+  end
+
+  def engine(raw_engine)
+    raw_engine.upcase
+  end
+
+  def version(raw_version)
+    view.number_with_precision(raw_version)
   end
 
 end
